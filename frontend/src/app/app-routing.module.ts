259640c14761fc92f80d06b47c93c390d0681115
Simon Leblanc <010.p0lly.010@gmail.com>
2018-06-28 12:01:34 -0400

    added timer still don't work

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SprintsComponent } from '../app/sprints/sprints.component';
 import { HomeComponent } from '../app/home/home.component';
+import { NewSprintWorkingComponent } from '../app/new-sprint-working/new-sprint-working.component';
+
 
 const routes: Routes = [
   { path: 'sprints', component: SprintsComponent},
   { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'running', component: NewSprintWorkingComponent},
+
+
 ];
 
 @NgModule({
--- frontend/src/app/new-sprint-working/new-sprint-working.component.ts
+++ frontend/src/app/new-sprint-working/new-sprint-working.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PastSprint } from '../../models/PastSprint';
+import { Status } from '../../models/Status';
+import { ViewChild, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-sprint-working',
@@ -7,7 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewSprintWorkingComponent implements OnInit {
 
-  constructor() { }
+  progressPercent: number;
+
+  sprint: PastSprint;
+
+  public onClick() {
+    let seconds = setInterval(() => {
+      this.sprint.progress++;
+      if (this.progressPercent < (Math.floor(((this.sprint.progress/this.sprint.duration)*100)))) {
+        this.progressPercent = Math.floor(((this.sprint.progress/this.sprint.duration)*100));
+      }
+    },999);
+
+    setTimeout(() => {
+      clearInterval(seconds);
+    },this.sprint.duration);
+  };
+
+  constructor() {
+    this.sprint = new PastSprint();
+    this.sprint.name = "mySprint";  
+    this.sprint.duration = 25000;
+    this.sprint.status = new Status("initial");
+
+    this.sprint.progress = 0;
+    this.sprint.description = "a simple timer";
+    this.sprint.notify = false;
+    this.sprint.user = 555;
+    this.progressPercent = 0;
+
+  }
 
   ngOnInit() {
   }
