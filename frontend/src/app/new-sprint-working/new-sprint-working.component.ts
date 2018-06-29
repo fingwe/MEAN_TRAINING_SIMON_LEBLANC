d6076b65edfc3ade37c0cd8031533d676645fd48
Simon Leblanc <010.p0lly.010@gmail.com>
2018-06-29 16:32:13 -0400

    affiné le sprint

--- frontend/src/app/new-sprint-working/new-sprint-working.component.ts
+++ frontend/src/app/new-sprint-working/new-sprint-working.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
 import { PastSprint } from '../../models/PastSprint';
 import { Status } from '../../models/Status';
-import { ViewChild, Renderer2, AfterViewInit } from '@angular/core';
+import { Interval } from '../../models/Interval';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
 
 @Component({
   selector: 'app-new-sprint-working',
@@ -10,27 +14,70 @@ import { ViewChild, Renderer2, AfterViewInit } from '@angular/core';
 })
 export class NewSprintWorkingComponent implements OnInit {
 
+  modalRef: BsModalRef;
+
   progressPercent: number;
+
+  interval: Interval;
+
+  finishedTemplate: TemplateRef<any>;
 
   sprint: PastSprint;
 
-  public onClick() {
-    let seconds = setInterval(() => {
-      this.sprint.progress++;
-      if (this.progressPercent < (Math.floor(((this.sprint.progress/this.sprint.duration)*100)))) {
-        this.progressPercent = Math.floor(((this.sprint.progress/this.sprint.duration)*100));
-      }
-    },999);
+  /**
+   * Method that listen to a click event and start a timer
+   * each seconds, adds one second to sprint.progess 
+   * and
+   * update progressPercent to the right percentage of progress
+   * 
+   * @param event the click event
+   */
+  onClickStart(event) {
+    this.timerStart();
+  }
 
-    setTimeout(() => {
-      clearInterval(seconds);
-    },this.sprint.duration);
-  };
+  onStop(event,stopDialog: TemplateRef<any>) {
+    this.timerStop();
+    this.modalRef = this.modalService.show(stopDialog);
+  }
 
-  constructor() {
+  onResume(event) {
+    this.modalService.hide(1);
+    this.timerRestart();
+  }
+
+  onAbort(event) {
+    this.modalService.hide(1)
+    this.timerAbort();
+    this.interval.isFinished = true;
+  }
+
+  onFinishedListenner() {
+    if (this.interval.isFinished) {
+      this.onFinished();
+    }
+   return this.interval.isFinished;
+  }
+
+  importFinishDialog(event,finished: TemplateRef<any>) {
+    this.finishedTemplate = finished;
+  }
+
+  onFinished() {
+
+    //this.modalRef = this.modalService.show(this.finishedTemplate);
+  }
+
+  /**
+   * 
+   * initialize the timer object
+   * 
+   * @param sprint a sprint object 
+   */
+  timerInit(sprint?: PastSprint) {
     this.sprint = new PastSprint();
     this.sprint.name = "mySprint";  
-    this.sprint.duration = 25000;
+    this.sprint.duration = 3000;
     this.sprint.status = new Status("initial");
 
     this.sprint.progress = 0;
@@ -38,6 +85,97 @@ export class NewSprintWorkingComponent implements OnInit {
     this.sprint.notify = false;
     this.sprint.user = 555;
     this.progressPercent = 0;
+    this.sprint.createdAt = new Date();
+    this.interval.isFinished = false;
+  }
+
+  /**
+   * Manage the work of the timer
+   */
+  timerWork() {
+    this.interval.runner = setInterval(()=>{
+      this.interval.isRunning = true;
+      let diff = Date.now()-(Date.parse(this.sprint.startedAt.toString())+this.sprint.progress);
+      this.sprint.progress+=diff;
+      if (this.sprint.progress < this.sprint.duration) {
+        this.progressPercent = parseFloat(Math.round((this.sprint.progress/this.sprint.duration)*100).toFixed(1));
+      } else {
+        this.sprint.progress = this.sprint.duration;
+        this.progressPercent = 100;
+        this.timerEnd();
+      }
+    },1);
+  };
+
+  /**
+   * Manage the start of the timer
+   */
+  timerStart() {
+    
+    if (this.interval.isRunning) {
+      this.timerEnd();
+      this.timerInit();
+    }
+    this.sprint.startedAt = new Date();
+    this.sprint.finishedAt = new Date(Date.parse(this.sprint.startedAt.toString())+this.sprint.duration);
+    this.timerWork();
+  }
+
+
+  /**
+   * Manage the stop of the timer
+   */
+  timerStop() {
+    clearInterval(this.interval.runner);
+    this.interval.isRunning = false;
+  }
+
+  /**
+   * Manage the restart of the timer
+   */
+  timerRestart() {
+    if (!this.interval.isRunning) {
+      this.timerWork();
+    }
+  }
+
+  /**
+   * Manage the abort of the timer
+   */
+  timerAbort() {
+    clearInterval(this.interval.runner);
+    this.interval.isRunning = false;
+    this.sprint.finishedAt = new Date();
+    this.interval.isFinished = true;
+    this.router.navigateByUrl('/sprints');
+
+  }
+
+  /**
+   * Manage the natural end of the timer
+   */
+  timerEnd() {
+    clearInterval(this.interval.runner);
+    this.interval.isRunning = false;
+    this.interval.isFinished = true;
+  }
+
+  /**
+   * Method that returns a formated progress for styling the progess bar progress
+   */
+  getProgress() {
+    return `${this.progressPercent}rem`;
+  }
+
+  /**
+   * Constructor method
+   * set the inital stub sprint for now
+   */
+  constructor(private modalService: BsModalService, private router: Router) {
+    this.interval = new Interval();
+    this.interval.isRunning = false;
+    this.timerInit();
+    this.timerStart();
 
   }
 
